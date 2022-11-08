3044b29819d9d224a845b8402c1b53145769c73b
evan70 <ivanpaldan@yandex.com>
2022-11-08 21:09:40 +0100

    new file:   resources/views/home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="content">
+        @if(count($success) > 0)
+            @foreach($success as $value)
+                <div class="alert alert-success" role="alert">
+                    {{ $value }}
+                </div>
+            @endforeach
+        @endif
+        <h1 class="text-center">{{ $title }}</h1>
+        @if($user)
+            <h2>Vitaj, {{ $user }}!</h2>
+        @endif
+        <div>{!! $content !!}</div>
+    </div>
+@endsection
