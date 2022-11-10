dbeb68d9f0b18dbd2abd7c6ade12a90145a85917
evan70 <ivanpaldan@yandex.com>
2022-11-10 23:40:12 +0100

    new file:   resources/views/include/header.blade.php

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <div class="nav-wrap border-bottom">
+                @include('include.logo')
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    @include('include.menu')
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
