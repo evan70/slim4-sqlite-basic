70ee65fe888b049ed77f8e316887d1b7bff5318e
evan70 <ivanpaldan@yandex.com>
2022-11-12 23:19:58 +0100

    new file:   config/database.php

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'driver'    => 'sqlite',
+    'database'  => '/home/evan/www/php/slim-basic-app-admin-panel/database/slim-app.db',
+    'charset'   => 'utf8',
+    'prefix'    => '',
+];
