88071cc172ab637cc13ad4b6e7337896200acea7
evan70 <ivanpaldan@yandex.com>
2022-11-12 23:19:33 +0100

    new file:   config/blade.php

---
+++ config/blade.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'cache' => __DIR__ . '/../tmp/cache-blade',
+    'views' => __DIR__ . '/../resources/views',
+    'mode' => 5 // one of this values: 0 (MODE_AUTO), 1 (MODE_SLOW), 2 (MODE_FAST), 5 (MODE_DEBUG)
+];
