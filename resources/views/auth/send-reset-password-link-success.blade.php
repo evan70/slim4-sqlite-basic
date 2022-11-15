daaa3f4bc35972744b9b9ab0a11c6b9971dbed89
evan70 <ivanpaldan@yandex.com>
2022-11-15 23:50:33 +0100

    new file:   resources/views/auth/send-reset-password-link-success.blade.php

---
+++ resources/views/auth/send-reset-password-link-success.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="content">
+        <h1>Zmena hesla</h1>
+        <div class="form-signin">
+            <div class="alert alert-success" role="alert">
+                Link na zmenu hesla Vám bol poslaný na Váš mail. Nasledujte pokyny v e-maile.
+            </div>
+        </div>
+    </div>
+@endsection
